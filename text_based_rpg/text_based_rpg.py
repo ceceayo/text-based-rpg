57e6ef6b007333b6b9d3be83a3d550fffc3a7799
ceayo <100067092+ceceayo@users.noreply.github.com>
2023-05-28 09:57:31 +0200

    works :)

--- text_based_rpg/text_based_rpg.py
+++ text_based_rpg/text_based_rpg.py
@@ -15,23 +15,7 @@ class State(pc.State):
 
 def index() -> pc.Component:
     return pc.center(
-        pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": "rgb(107,99,246)",
-                },
-            ),
-            spacing="1.5em",
-            font_size="2em",
-        ),
-        padding_top="10%",
+        pc.text("hi")
     )
 
 
