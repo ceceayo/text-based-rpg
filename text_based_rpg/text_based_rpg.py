9059be26420da7be7fa36461020e21504b28bc95
ceayo <100067092+ceceayo@users.noreply.github.com>
2023-05-28 14:24:27 +0200

    added keys for user to the state.

--- text_based_rpg/text_based_rpg.py
+++ text_based_rpg/text_based_rpg.py
@@ -1,6 +1,7 @@
 """Welcome to Pynecone! This file outlines the steps to create a basic app."""
 from pcconfig import config
-
+import random
+import string
 import pynecone as pc
 
 docs_url = "https://pynecone.io/docs/getting-started/introduction"
@@ -9,6 +10,21 @@ filename = f"{config.app_name}/{config.app_name}.py"
 
 class State(pc.State):
     """The app state."""
+
+    """keys part
+    a user has two keys, consisting of 64 bits of random nonsense,
+    with which the user can be identified in the database.
+    """
+    key1: str
+    key2: str
+
+    def generate_new_key_pair(self):
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for _ in range(64))
+        self.key1 = result_str
+        del result_str
+        result_str = ''.join(random.choice(letters) for _ in range(64))
+        self.key2 = result_str
 
     pass
 
