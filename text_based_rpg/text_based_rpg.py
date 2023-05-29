d50486c64556c49ce56b32f492391514eaf35eb8
luxkatana <taseen.bibi@gmail.com>
2023-05-29 22:07:20 +0200

    added states.py & custom 404 page

--- text_based_rpg/text_based_rpg.py
+++ text_based_rpg/text_based_rpg.py
@@ -1,75 +1,49 @@
 """Welcome to Pynecone! This file outlines the steps to create a basic app."""
-import  secrets
-import string
 import pynecone as pc
-
-
-
-class BaseState(pc.State):
-    '''
-    Represents the BaseState, this makes it possible in the future to create multiple States.
-    Inherit from BaseState
-    '''
-    ...
-class KeyGenerationState(BaseState):
-
-    """
-    keys part
-    a user has two keys, consisting of 64 bits of random nonsense,
-    with which the user can be identified in the database.
-    """
-    key1: str
-    key2: str
-
-    options: list[str]
-    selected_option: str
-
-    logs: list[str]
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.generate_new_key_pair()
-        self.options = ['a', 'b']
-        self.logs += ['hi', 'it works!']
-
-
-    def generate_new_key_pair(self):
-        letters = string.ascii_lowercase
-        result_str = ''.join(secrets.choice(letters) for _ in range(64))
-        self.key1 = result_str
-        del result_str
-        result_str = ''.join(secrets.choice(letters) for _ in range(64))
-        self.key2 = result_str
-
-    def use_option(self, option):
-        if option not in self.options: raise "option not in self.options!"
-        self.logs.append(f"used option {option}.")
-
-
-
-
-def index() -> pc.Component:
+from .states import *
+def debugging() -> pc.Component:
     return pc.center(
         pc.vstack(
-            pc.text("1 is " + KeyGenerationState.key1),
-            pc.text("2 is " + KeyGenerationState.key2),
+            pc.text("1 is " + UserInformation.key1),
+            pc.text("2 is " + UserInformation.key2),
             pc.stack(
-                pc.foreach(KeyGenerationState.logs, lambda item: pc.text(item)),
+                pc.foreach(LoggingState.logs, lambda item: pc.text(item)),
                 width="55%",
                 bg="#AAAAAA",
                 padding="8px",
                 border_radius="4px",
             ),
             pc.hstack(
-                pc.foreach(KeyGenerationState.options, lambda x: pc.button(x,
-                                                                           on_click=lambda: KeyGenerationState.use_option(x)))
+                pc.foreach(LoggingState.options, lambda x: pc.button(x,
+                                                                           on_click=lambda: LoggingState.use_option(x)))
             ),
             width="100%"
         ),
         width="100%"
     )
-
+def start_game() -> pc.Component:
+    if not UserInformation._name_legal:
+        return pc.stack(
+            pc.center(
+                pc.input(
+                    placeholder="name here",
+                    on_change=UserInformation.change_name,
+                    width='50%',
+                ),
+            ),
+            pc.vstack(
+                pc.heading("Welcome " + UserInformation.name)
+            )
+            )
+    return pc.heading("hello there")
+def page404() -> pc.Component:
+    return pc.center(
+        pc.heading(pc.link("This page does not exist(404)", href='https://youtu.be/xvFZjo5PgG0'))
+        )
 
 # Add state and page to the app.
 app = pc.App(state=BaseState)
-app.add_page(index)
+app.add_page(debugging, '/logging/')
+app.add_page(start_game, '/start/')
+app.add_custom_404_page(page404)
 app.compile()
