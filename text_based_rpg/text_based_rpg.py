25a72f2a729dea080e2dd5e378e6ae71104d93b3
luxkatana <taseen.bibi@gmail.com>
2023-05-29 15:04:19 +0200

    Made some refactorings

--- text_based_rpg/text_based_rpg.py
+++ text_based_rpg/text_based_rpg.py
@@ -1,41 +1,43 @@
 """Welcome to Pynecone! This file outlines the steps to create a basic app."""
-from pcconfig import config
-import random
+import  secrets
 import string
 import pynecone as pc
-from typing import List
-
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
-filename = f"{config.app_name}/{config.app_name}.py"
 
 
-class State(pc.State):
-    """The app state."""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.generate_new_key_pair()
-        self.options = ['a', 'b']
-        self.logs += ['hi', 'it works!']
+class BaseState(pc.State):
+    '''
+    Represents the BaseState, this makes it possible in the future to create multiple States.
+    Inherit from BaseState
+    '''
+    ...
+class KeyGenerationState(BaseState):
 
-    """keys part
+    """
+    keys part
     a user has two keys, consisting of 64 bits of random nonsense,
     with which the user can be identified in the database.
     """
     key1: str
     key2: str
 
-    options: List[str]
+    options: list[str]
     selected_option: str
 
-    logs: List[str]
+    logs: list[str]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.generate_new_key_pair()
+        self.options = ['a', 'b']
+        self.logs += ['hi', 'it works!']
+
 
     def generate_new_key_pair(self):
         letters = string.ascii_lowercase
-        result_str = ''.join(random.choice(letters) for _ in range(64))
+        result_str = ''.join(secrets.choice(letters) for _ in range(64))
         self.key1 = result_str
         del result_str
-        result_str = ''.join(random.choice(letters) for _ in range(64))
+        result_str = ''.join(secrets.choice(letters) for _ in range(64))
         self.key2 = result_str
 
     def use_option(self, option):
@@ -43,24 +45,23 @@ class State(pc.State):
         self.logs.append(f"used option {option}.")
 
 
-def render_log_item(log_item):
-    return pc.text(log_item)
 
 
 def index() -> pc.Component:
     return pc.center(
         pc.vstack(
-            pc.text("1 is " + State.key1),
-            pc.text("2 is " + State.key2),
+            pc.text("1 is " + KeyGenerationState.key1),
+            pc.text("2 is " + KeyGenerationState.key2),
             pc.stack(
-                pc.foreach(State.logs, render_log_item),
+                pc.foreach(KeyGenerationState.logs, lambda item: pc.text(item)),
                 width="55%",
                 bg="#AAAAAA",
                 padding="8px",
                 border_radius="4px",
             ),
             pc.hstack(
-                pc.foreach(State.options, lambda x: pc.button(x, on_click=lambda _: State.use_option(x)))
+                pc.foreach(KeyGenerationState.options, lambda x: pc.button(x,
+                                                                           on_click=lambda: KeyGenerationState.use_option(x)))
             ),
             width="100%"
         ),
@@ -69,6 +70,6 @@ def index() -> pc.Component:
 
 
 # Add state and page to the app.
-app = pc.App(state=State)
+app = pc.App(state=BaseState)
 app.add_page(index)
 app.compile()
